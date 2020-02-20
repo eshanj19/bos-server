e3cede9ae20f44c2dfb1947e680d8d7f53f8cb9c
anaghagodbole <anaghagodbole10@gmail.com>
2020-02-20 14:19:29 +0530

    translation change

--- bos/conv.py
+++ bos/conv.py
@@ -126,7 +126,7 @@ _("ra.title.uom"),
 
 _("ra.title.inputtype"),
 
-_("ra.title.measurement"),
+_("ra.title.measurements"),
 
 _("ra.title.athlete"),
 
@@ -148,7 +148,7 @@ _("ra.menu.reading"),
 
 _("ra.menu.measurement_type"),
 
-_("ra.menu.ngo"),
+_("ra.menu.ngos"),
 
 _("ra.title.set_parent"),
 
@@ -156,7 +156,184 @@ _("ra.title.names_of_permission_group"),
 
 _("ra.title.is_active"),
 
-_("ra.title.view_edit_organisation")
+_("ra.title.view_edit_organisation"),
+
+_("ra.theme.light"),
+
+_("ra.theme.dark"),
+
+_("Create Measurement type"),
+
+_("ra.title.recorded_on"),
+
+_("ra.action.next"),
+
+_("applicant's details"),
+
+_("ra.language.en"),
+
+_("ra.language.hi"),
+
+_("configuration"),
+
+_("logout"),
+
+_("login"),
+
+_("ra.menu.admins"),
+
+_("ra.menu.requests"),
+
+_("List of user groups"),
+
+_("List of permission groups"),
+
+_("ra.title.role"),
+
+_("ra.title.date"),
+
+_("ra.title.status"),
+
+_("ra.action.mandatory"),
+
+_(" Create Curriculum"),
+
+_("Create Training Session"),
+
+_("Create File Resource"),
+
+_("Create Registration Form"),
+
+_("ra.action.add_measurement"),
+
+_("ra.action.add_file"),
+
+_("ra.action.select_file"),
+
+_("Edit curriculum")
+
+_("Edit training session"),
+
+_("Edit file resources"),
+
+_("Edit registration form"),
+
+_("Select a file to upload"),
+
+_(" Set As Athlete Registration"),
+
+_("Set As Coach Registration"),
+
+_("ra.action.activate"),
+
+_("a.action.deactivate"),
+
+_("Members with no athlete/coach relationship"),
+
+_('ra.action.reset_password'),
+
+_("ra.action.cancel"),
+
+_("ra.action.reset"),
+
+_("ra.title.no_members"),
+
+_("ra.title.empty"),
+
+_("ra.menu.coaches"),
+
+_("ra.menu.organisation"),
+
+_("Welcome to Bridges of Sports platform"),
+
+_("edit ngo"),
+
+_("create ngo"),
+
+_("create admin"),
+
+_("edit admin")
+
+_("edit coach"),
+
+_("create coach")
+
+_("edit athelete"),
+
+_("create athlete"),
+
+_("create user group"),
+
+_("edit user group")
+
+_("create request"),
+
+_("edit request"),
+
+_("edit measurements"),
+
+_("create measuremet"),
+
+_("ra.action.accept"),
+
+_('ra.action.decline'),
+
+_("ra.action.set"),
+
+_("Choose username and password"),
+
+_("ra.title.enter_password"),
+
+_("ra.title.curriculum_name"),
+
+_("ra.title.curriculum_description"),
+
+_("ra.title.enter_day_title"),
+
+_("ra.action.add_session"),
+
+_("ra.action.add_day"),
+
+_("ra.title.enter_item_title"),
+
+_("ra.title.file_name"),
+
+_("ra.title.file_description"),
+
+_("file resource")
+
+_("Permissions for group"),
+
+_("admin_information"),
+
+_("coach_information"),
+
+_("athlete_information"),
+
+_("create measurement type"),
+
+_("edit measurement type"),
+
+_("Enter Session Title"),
+
+_("measurement information"),
+
+_("Edit Reading"),
+
+_("ra.title.theme"),
+
+_("ra.title.language")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
